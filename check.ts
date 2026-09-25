d3a26e07eb664e77b2e1eebf814ceb542c838526
Assiya Bykayeva <assiya.sabyrova@gmail.com>
2026-09-25 10:11:51 +0500

    chore: remove temp script

--- check.ts
+++
@@ -1,8 +0,0 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-import { prisma } from './src/lib/db';
-async function main() {
-    const users = await prisma.user.findMany({ select: { name: true, login: true, password: true } });
-    console.log(users.slice(0, 5));
-}
-main().finally(() => prisma.$disconnect());
